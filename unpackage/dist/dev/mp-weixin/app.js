283c18b3dc359acd946212312a3d90a2658eb780
HeDong Zhang <zhanghedong2012@126.com>
2025-05-17 15:27:19 +0800

    init

---
+++ unpackage/dist/dev/mp-weixin/app.js
@@ -0,0 +1,33 @@
+"use strict";
+Object.defineProperty(exports, Symbol.toStringTag, { value: "Module" });
+const common_vendor = require("./common/vendor.js");
+if (!Math) {
+  "./pages/index/index.js";
+  "./pages/me/me.js";
+  "./pages/message/message.js";
+  "./pages/find/find.js";
+  "./pages/publish/publish.js";
+}
+const _sfc_main = common_vendor.defineComponent(new UTSJSONObject({
+  onLaunch: function() {
+    common_vendor.index.__f__("log", "at App.uvue:5", "App Launch");
+  },
+  onShow: function() {
+    common_vendor.index.__f__("log", "at App.uvue:8", "App Show");
+  },
+  onHide: function() {
+    common_vendor.index.__f__("log", "at App.uvue:11", "App Hide");
+  },
+  onExit: function() {
+    common_vendor.index.__f__("log", "at App.uvue:32", "App Exit");
+  }
+}));
+function createApp() {
+  const app = common_vendor.createSSRApp(_sfc_main);
+  return {
+    app
+  };
+}
+createApp().app.mount("#app");
+exports.createApp = createApp;
+//# sourceMappingURL=../.sourcemap/mp-weixin/app.js.map
